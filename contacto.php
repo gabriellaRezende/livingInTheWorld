1b27057568e55b876be3fbfcb900d610bfb5b4c6
gabriellaRezende <gabriella.rubinrez@hotmail.com>
2024-07-03 20:41:18 +0100

    page contacto

---
+++ contacto.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Cidadania Matos </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="./CSS/styles.css">
+
+    <style>
+
+    </style>
+</head>
+
+
+
+<body class="vh-100 d-flex flex-column justify-content-between align-itens-center">
+
+    <div>
+        <!-- header -->
+        <?php include "header.php"; ?>
+    </div>
+
+    <div class="">
+
+        <div class="card w-100 mb-5 mt-5 ">
+            <div class="card-body d-flex">
+                <div class="flex-fill">
+                    <h1 class="card-title">Fale com a gente!!!</h1>
+                    <p class="card-text"> Qualquer coisa para explicar como va ser feita a resposta e o envio da questão. </p>
+                </div>
+
+                <div class="col-md-4 ">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Assunto</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Dúvidas e Reclamações</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <a href="404page.php" class="btn btn-primary">Enviar</a>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Accordion Item #1
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Accordion Item #2
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Accordion Item #3
+                </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <!-- Footer -->
+        <?php include "footer.php"; ?>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -1,28 +1,26 @@
-
-
-
-
-<nav class="navbar navbar-expand-lg fixed-top mb-5" style="background-color: var(--cor-Bgcomponente)";>
-  <div class="container-fluid" style="">
-    <a class="navbar-brand me-4" href="#">
-      <img src="./images/logo CR.svg" alt="Cidadania Matos" width="30" height="30">
-      Cidadania Matos
-    </a>
-    <button class="navbar-toggler border-0 " style="" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <i class="bi bi-list"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
-          <a class="nav-link h1" style="" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="about.php">Sobre Nós</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Serviços</a>
-        </li>
-        <!-- <li class="nav-item dropdown">
+  
+  
+  <nav class="navbar navbar-expand-lg fixed-top mb-5" style="background-color: var(--cor-Bgcomponente)" ;>
+    <div class="container-fluid" style="">
+      <a class="navbar-brand me-4" href="#">
+        <img src="./images/logo CR.svg" alt="Cidadania Matos" width="30" height="30">
+        Cidadania Matos
+      </a>
+      <button class="navbar-toggler border-0 " style="" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="bi bi-list"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link h1" style="" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="about.php">Sobre Nós</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="servicos.php">Serviços</a>
+          </li>
+          <!-- <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
@@ -33,15 +31,24 @@
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li> -->
-        <li class="nav-item">
-          <a class="nav-link " aria-disabled="true">Contato</a>
-        </li>
-      </ul>
+          <li class="nav-item">
+            <a class="nav-link " aria-disabled="true" href="contacto.php">Contato</a>
+          </li>
+        </ul>
 
-      <div class="d-flex" role="search">
-      <a href="login.php" class="btn">Login</a>
-        <button type="button" class="btn"> <i class="bi bi-person-circle"></i> </button>
+        <div class="d-flex" role="search">
+          <a href="login.php" class="btn">Login</a>
+          <button type="button" class="btn"> <i class="bi bi-person-circle"></i> </button>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
+
+
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Library</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Data</li>
+    </ol>
+  </nav>
